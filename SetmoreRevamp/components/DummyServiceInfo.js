d58406fe8e0df75004be6d23d1c73e3a060d3955
Xiaoyun Gabrielle Bonato <46456555+xiaoyunbonato@users.noreply.github.com>
2023-03-22 20:36:42 -0400

    Create DummyServiceInfo.js
    it won't let me make one either- only files

---
+++ SetmoreRevamp/components/DummyServiceInfo.js
@@ -0,0 +1,88 @@
+{
+
+"businessName": "Mochi Hair Co.", 
+
+"about": "Welcome to Mochi Hair Co., the ultimate luxury hair salon experience. Our salon boasts a sleek and modern interior design, with plush leather chairs and marble countertops to make you feel like royalty from the moment you step inside.
+Our team of expert stylists are all trained at prestigious hair academies in Paris, New York, and Milan, and are up-to-date with the latest hair trends and techniques. We use only the highest quality products from luxury brands like Oribe, Bumble and Bumble, and Kerastase, ensuring that your hair receives the best care possible.
+As you sit back and relax, sipping on a glass of champagne, our stylists will consult with you to determine the perfect hairstyle for your face shape and personal style. From balayage highlights to intricate updos, we will make sure you leave feeling confident and beautiful.
+At Mochi Hair Co., we understand that your time is valuable, which is why we offer a range of premium services, including a private styling room for VIP clients and a concierge service that will take care of your every need.
+Experience the ultimate indulgence at Mochi Hair Co. and let us transform your hair into a work of art.", 
+
+"reviews":4.7, 
+"services": ["Cutting & Styling , Hair Colouring, 
+Extensions Treatments"],
+
+"staff": ["Angela, David"]
+
+}
+
+{
+
+"businessName": "Skin Artistry Medical Aesthetics",
+
+"about": "Welcome to "Skin Artistry Medical Aesthetics," the premier destination for luxury skincare treatments. Our skin spa offers a serene and calming atmosphere with soothing music, soft lighting, and elegant decor to provide you with a truly relaxing experience.
+Our team of licensed estheticians has years of experience working with clients of all skin types, ensuring that you receive personalized and effective treatments. We use only the highest quality skincare products from luxury brands like Zo Skin, Biologique Recherche, and Skinceuticals to achieve radiant and youthful-looking skin.
+From our signature facial treatments to our full-body massage, we offer a range of services designed to pamper and rejuvenate your skin. Our advanced skincare technology includes LED light therapy, microdermabrasion, and radiofrequency treatments, among others, to provide you with the latest and most effective skincare solutions.
+At Skin Artistry Medical Aesthetics, we prioritize your comfort and relaxation, which is why we offer luxurious amenities like plush robes, heated massage tables, and complimentary refreshments. Our experienced and friendly staff will take care of your every need, ensuring that you leave feeling refreshed and rejuvenated.
+Indulge in the ultimate pampering experience at Skin Artistry Medical Aesthetics skin spa, and let us help you achieve the glowing, healthy skin you deserve.",
+
+"reviews": 4.9,
+
+"services": ["HydraFacial", "PRP", "Cool Peel"],
+
+"staff"": ["Sae Eun", "Alyssa", "Rachel"
+
+}
+
+{
+
+"businessName": "Majesty Nail Bar",
+
+"about": "Welcome to "Majesty Nail Bar," the ultimate destination for luxury nail services. Our nail bar provides a chic and modern atmosphere where you can relax and indulge in a range of nail treatments.
+Our team of skilled nail technicians are trained to provide you with personalized and professional nail services using only the highest quality nail products from luxury brands like OPI, Chanel, and Essie. From classic manicures and pedicures to gel and acrylic extensions, we offer a wide range of services to meet your every nail need.
+At Majesty Nail Bar, we understand that hygiene is essential, which is why we adhere to the highest standards of cleanliness and sanitation. We use disposable tools and sterilize all equipment after each use to ensure that your experience is safe and comfortable.
+Our luxurious amenities include heated massage chairs, complimentary refreshments, and a wide selection of nail polish colors to choose from. Whether you're looking for a quick touch-up or a full pampering session, we have you covered.
+Experience the ultimate in luxury nail services at Majesty Nail Bar, where we strive to make your nail care experience as relaxing and enjoyable as possible.",
+
+"reviews": 4.5,
+
+"staff": "Jenna", "Chloe"
+
+}
+
+{
+
+"businessName": "Takashi's Pottery",
+
+"about": "Welcome to "Takashi's Pottery," the premier destination for luxury pottery classes. Our studio offers a serene and creative atmosphere where you can learn the art of pottery and explore your creativity.
+Our team of experienced instructors are dedicated to providing you with personalized and professional pottery classes. Whether you're a beginner or an experienced potter, we offer a range of classes tailored to meet your needs, from hand-building techniques to wheel throwing.
+We use only the highest quality clay and pottery tools from luxury brands like Amaco and Skutt to ensure that you have the best materials to work with. Our studio is equipped with state-of-the-art pottery wheels and kilns to give you the most authentic and rewarding pottery experience.
+At Takashi's Pottery, we believe that pottery is not just a craft, but a form of therapy. That's why we prioritize your comfort and relaxation by providing luxurious amenities like plush seating, complimentary refreshments, and soothing music.
+Join us at Takashi's Pottery, and let our experienced instructors guide you on a journey of creativity and self-discovery through the art of pottery.",
+
+"reviews": 5.0,
+
+"services": ["3 Hour Session", "4x Weekly 1 Hour Sessions"],
+
+"staff": "Takashi"
+
+}
+
+{
+
+"businessName": "Pastry Alchemist",
+
+"about": "At Pastry Alchemist, our team of pastry chefs boasts an impressive background of experience working at Michelin-star bakeries around the world. Our chefs have honed their skills under the guidance of renowned pastry chefs, and have created stunning desserts for some of the world's most prestigious restaurants and hotels.
+Their experience includes working at Michelin-starred establishments like Pierre Hermé in Paris, Mugaritz in Spain, and The Fat Duck in the UK. They bring with them a wealth of knowledge and expertise, and are committed to sharing their passion for pastry with our students.
+At Pastry Alchemist, we are proud to have such talented pastry chefs on our team. They bring a level of skill and artistry to our classes that is unmatched, and they are dedicated to helping our students create beautiful and delicious",
+
+"reviews": 4.9,
+
+"services": ["3 Hour Session", "4x Weekly 2 Hour Sessions"],
+
+"staff": ["Ophélie", "Mathilde", [Clément"]
+
+}
+
+
+
