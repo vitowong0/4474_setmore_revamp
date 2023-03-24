afd5cc3568f1cc80b66b37566f50f0f5239cdde7
vitowong0 <101366834+vitowong0@users.noreply.github.com>
2023-03-24 18:04:37 -0400

    Dummy data!

---
+++ SetmoreRevamp/components/models/ServiceInfoOverview.js
@@ -0,0 +1,13 @@
+class ServiceInfoOverview {
+  constructor(id, name, description, rating, services, staff, timesAvailable) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.rating = rating;
+    this.services = services;
+    this.staff = staff;
+    this.timesAvailable = timesAvailable;
+  }
+}
+
+export default ServiceInfoOverview;
