e601fbfd1dbc065fc050a031434b149e48dbc896
gonzalez-chris <77905782+gonzalez-chris@users.noreply.github.com>
2023-03-29 17:23:15 -0400

    Create TimeButton & Calendar components

--- SetmoreRevamp/constants/styles.js
+++ SetmoreRevamp/constants/styles.js
@@ -3,13 +3,18 @@ export const Colours = {
   lightturqouise: '#B3D5CF',
   lightgreen: '#BFD6BD',
   pastelyellow: '#FBE098',
+  lightgrey: '#C0C0C0',
   mediumgrey: '#5E5E5E',
   westernpurple: '#4F2683',
   westerngrey: '#807F83',
+  lightergrey: '#E5E5E5',
 
   primary100: 'white',
   primary500: 'white',
   primary800: 'white',
   error100: '#fcdcbf',
   error500: '#f37c13',
+
+  mediumorange: '#FF7960',
+  pastelpink: '#FFD2CA',
 };
