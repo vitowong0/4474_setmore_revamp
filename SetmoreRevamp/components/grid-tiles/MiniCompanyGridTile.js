0e358d61f66a5c6dae8a8dea4dcd5dd903ac5f21
vitowong0 <101366834+vitowong0@users.noreply.github.com>
2023-03-30 13:22:41 -0400

    Rendering mini grid tiles on the home screen

---
+++ SetmoreRevamp/components/grid-tiles/MiniCompanyGridTile.js
@@ -0,0 +1,164 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Alert,
+} from 'react-native';
+import {Colours} from '../../constants/styles';
+import {useNavigation} from '@react-navigation/native';
+
+import dotdotdot from '../../assets/icons/dotdotdot.png';
+import whiteheartfill from '../../assets/icons/white_heart_fill.png';
+import whiteheartoutline from '../../assets/icons/white_heart_outline.png';
+
+function MiniCompanyGridTile({
+  id,
+  name,
+  image,
+  description,
+  rating,
+  services,
+  staff,
+  timesAvailable,
+  duration,
+  cost,
+}) {
+  const navigation = useNavigation();
+
+  const createTwoButtonAlert = () => {
+    Alert.alert('Alert Title', 'My Alert Msg', [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {text: 'OK', onPress: () => console.log('OK Pressed')},
+    ]);
+  };
+
+  return (
+    <View style={styles.root}>
+      <View style={styles.gridTileContainer}>
+        <View style={styles.topRow}>
+          <View style={{alignItems: 'flex-start'}}>
+            <Text style={styles.titleText}>{name}</Text>
+          </View>
+          <View>
+            <TouchableOpacity
+              onPress={() => {
+                alert('hide and shit');
+              }}>
+              <Image style={styles.dotdotdot} source={dotdotdot} />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.imageContainer}>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate('CompanyDetailsScreen');
+            }}>
+            <Image style={styles.image} source={image} />
+            <TouchableOpacity
+              onPress={() => {
+                alert('Remove from favourites?');
+              }}>
+              <Image style={styles.whiteHeart} source={whiteheartfill} />
+            </TouchableOpacity>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+export default MiniCompanyGridTile;
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+
+  gridTileContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    // paddingTop: 5,
+    paddingBottom: 10,
+    // paddingRight: 5,
+    paddingLeft: 20,
+  },
+
+  topRow: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    paddingVertical: 5,
+    paddingHorizontal: 8,
+    marginBottom: 2,
+  },
+
+  titleText: {
+    fontSize: 16,
+    fontWeight: 700,
+    color: Colours.darkturqouise,
+  },
+
+  dotdotdot: {
+    height: 18,
+    width: 35,
+    opacity: 0.5,
+  },
+
+  imageContainer: {
+    flex: 1,
+  },
+
+  image: {
+    width: 260,
+    height: 200,
+    borderRadius: 30,
+  },
+
+  whiteHeart: {
+    position: 'absolute',
+    width: 20,
+    height: 20,
+    bottom: 15,
+    right: 15,
+  },
+});
+
+// const serviceItems = ServiceInfo.map(service => {
+//   const serviceOverview = new ServiceInfoOverview(
+//     service.id,
+//     service.name,
+//     service.image,
+//     service.description,
+//     service.rating,
+//     service.services,
+//     service.staff,
+//     service.timesAvailable,
+//     service.duration,
+//     service.cost,
+//   );
+
+//   return (
+//     <TouchableOpacity>
+//       <View style={styles.test}>
+//         <View key={service.id}>
+//           <Text>{serviceOverview.name}</Text>
+//           <Text>{serviceOverview.description}</Text>
+//           <Text>Rating: {serviceOverview.rating}</Text>
+//           <Text>Services: {serviceOverview.services.join(', ')}</Text>
+//           <Text>Staff: {serviceOverview.staff.join(', ')}</Text>
+//           <Text>
+//             Times Available: {serviceOverview.timesAvailable.join(', ')}
+//           </Text>
+//           <Text>Duration: {serviceOverview.duration.join(', ')}</Text>
+//           <Text>Cost: {serviceOverview.cost.join(', ')}</Text>
+//         </View>
+//       </View>
+//     </TouchableOpacity>
+//   );
+// });
