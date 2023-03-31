010063cd0813cd4e62c715b942fbcef0baf229b4
Xiaoyun Gabrielle Bonato <46456555+xiaoyunbonato@users.noreply.github.com>
2023-03-31 16:53:21 -0400

    UPDATE: NearbyCompanyOverview, DUMMYDATA
    errr thing updated plus new images slay

---
+++ SetmoreRevamp/components/models/NearbyCompanyOverview.js
@@ -0,0 +1,23 @@
+class NearbyCompanyOverview {
+  constructor(
+    id,
+    name,
+    image,
+    rating,
+    distance,
+    dollarSign,
+    style,
+    timesAvailable,
+  ) {
+    this.id = id;
+    this.name = name;
+    this.image = image;
+    this.rating = rating;
+    this.distance = distance;
+    this.dollarSign = dollarSign;
+    this.style = style;
+    this.timesAvailable = timesAvailable;
+  }
+}
+
+export default NearbyCompanyOverview;
